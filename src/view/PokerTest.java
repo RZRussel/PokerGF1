0407a3ba37eba9ee7957967d4bf94a54969cbece
Russel <emkil.minor@gmail.com>
2016-07-19 21:22:04 +0300

    [fix] instructions for bet and all in actions added

--- src/view/PokerTest.java
+++ src/view/PokerTest.java
@@ -1,13 +1,10 @@
 package view;
 
-//import controller.GameEngine;
+import controller.GameEngine;
 
 public class PokerTest {
 	public static void main(String[] args){
-		//GameEngine gameEngine = new GameEngine();
-		//gameEngine.startGame(5000, 100);
-
-        GameInterface gi = new GameInterface();
-        gi.Test();
+		GameEngine gameEngine = new GameEngine();
+		gameEngine.startGame(5000, 100);
 	}
 }
